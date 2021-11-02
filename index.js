8e6ba9d5b55c7f6e21ef79bfc836f6dae4f258f7
Adam <ashaw315@gmail.com>
2021-11-02 11:42:21 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerUp() {
+    const bottomNumbers = dodger.style.bottom.replace("px","");
+    const bottom = parseInt(bottomNumbers, 10);
+
+    if (bottom < 380) {
+        dodger.style.bottom = `${bottom + 1}px`;
+    }
+
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    }
+});
